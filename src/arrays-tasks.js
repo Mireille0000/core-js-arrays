03b77eee879dea5885b4f951ca1817ec9be66225
Gasanova Tamara <solas93@mail.ru>
2024-01-21 02:09:04 +0300

    feat: implement toArrayOfSquares and getSecondItems functions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -214,8 +214,8 @@ function toCsvText(/* arr */) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  return arr.map((item) => item * item);
 }
 
 /**
@@ -247,8 +247,13 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.reduce((acc, item, index) => {
+    if (index % 2 > 0) {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
 }
 
 /**
