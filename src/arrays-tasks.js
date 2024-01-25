5c0abdef018484a8827614e411e4047c1bc87180
Gasanova Tamara <solas93@mail.ru>
2024-01-26 00:38:03 +0300

    feat: implement getPositivesCount function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -304,8 +304,9 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  const positiveNumArr = arr.filter((num) => Number.isInteger(num) && num > 0);
+  return positiveNumArr.length;
 }
 
 /**
