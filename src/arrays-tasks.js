79a1dd8d78c7b3f0743458a3771385561833b2af
Gasanova Tamara <solas93@mail.ru>
2024-01-26 15:43:17 +0300

    feat: impelement getFalsyValuesCount function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -358,8 +358,14 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((acc, item) => {
+    let accum = acc;
+    if (!item) {
+      accum += 1;
+    }
+    return accum;
+  }, 0);
 }
 
 /**
